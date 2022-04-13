5bf030ac5bbf3af640fe4483921f3108e422851e
HyungchulCha <ckgudcjf@gmail.com>
2022-04-13 22:55:49 +0900

    ing

---
+++ webpack/02/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div class='container'>
+      <h1 class='title'>Hi</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
